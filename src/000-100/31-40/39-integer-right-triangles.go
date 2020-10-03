d6294bddc17d8bbcebd6659ce4699d0406d2a9fb
klvenky <k.leelavenkatesh@gmail.com>
2020-10-04 01:14:36 +0530

    improved some sample logic

--- src/000-100/31-40/39-integer-right-triangles.go
+++ src/000-100/31-40/39-integer-right-triangles.go
@@ -20,45 +20,29 @@ func main() {
 	limit := 1000
 	for perimeter := 1; perimeter <= limit; perimeter++ {
 		var points [][]int
-		// perimeter := 120
 		for a := 1; a <= perimeter; a++ {
 			bMax := perimeter - a
 			for b := a; b <= bMax; b++ {
 				cMax := bMax - b
 				for c := b; c <= cMax; c++ {
-					isRight := isRightTriangle(a, b, c, perimeter)
-					if isRight {
-						//fmt.Println("a: ", a, "| b: ", b, "| c: ", c, " | p: ", perimeter)
-						//fmt.Printf("{ %d, %d, %d }\n", a, b, c)
+					if isRightTriangle(a, b, c, perimeter) {
 						tmp := []int{a, b, c}
-						//fmt.Println("---before sort --- ", tmp)
 						sort.Ints(tmp)
-						//fmt.Println("---after sort --- ", tmp)
-
 						eq := 0
 						push := false
 						if len(points) == 0 {
 							push = true
 						} else {
-							//fmt.Println("in else ", len(points), " tmp -> ", tmp)
 							for index, _ := range points {
-
 								for tmpi, _ := range tmp {
 									if tmp[tmpi] == points[index][tmpi] {
 										eq++
 									}
 								}
-
-								if eq == 0 {
-									push = true
-								} else {
-									push = false
-								}
-								//fmt.Println("index -> ", index, " --- equal ->  ", eq, " --- push --> ", push)
+								push = eq == 0
 							}
 						}
 						if push {
-							//fmt.Println(" ---- pushing ---- ", tmp)
 							points = append(points, tmp)
 						}
 					}
@@ -70,8 +54,6 @@ func main() {
 			maxSolutions = len(points)
 			currMaxPerimeter = perimeter
 		}
-		// fmt.Println("perimeter : ", perimeter, " | solutions : ", maxSolutions)
-
 	}
 	fmt.Println("max perimter ---- ", currMaxPerimeter, "\nmax solutions", maxSolutions)
 
